b35e74556b731cc37abc7e6a3306ff201765aa61
unknown <you@example.com>
2025-04-15 08:26:17 -0300

    criei um algoritmo que peça para voce colocar um numero e ele vai ver se o numero é positivo ou negativo

---
+++ index.js
@@ -0,0 +1,12 @@
+// digite um numero
+let numero = prompt("Digite um número:");
+
+// converte o valor digitado
+numero = Number(numero);
+
+// Verifica se o número é maior que zero
+if (numero > 0) {
+  alert("Número positivo.");
+} else {
+  alert("Número não é positivo.");
+}
